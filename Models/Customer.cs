2636d956cc36185e3ce3ba52b751fbe14506290e
emmanuel <emmanuelntibimenya@gmail.com>
2022-05-18 21:18:31 +0100

    added validation to customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace AccountManagement.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-
+        [Required]
         public string? FullName { get; set; }
-
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
-        
+        [Required]
+        [Phone]
         public string? Phone { get; set; }
-
+        [Required]
         public string? Address { get; set; }
         public Customer()
         {
