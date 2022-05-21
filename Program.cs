86cc986da4821e293d4e52ac6f59e2818de38b1c
emmanuel <emmanuelntibimenya@gmail.com>
2022-05-21 15:01:15 +0100

    implemented an automatic database migration in program.cs which is the entry file of the application

--- Program.cs
+++ Program.cs
@@ -41,4 +41,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dataContext.Database.Migrate();
+}
+
 app.Run();
---
+++ Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountManagement.Utility
+{
+    public class SD
+    {
+        public const string AdminEndUser = "Admin";
+        public const string CustomerEndUser = "Customer";
+
+    }
+}
